151257f71167cb688908fcbc85483dc67d0ee0e4
Klim Kornienko <yorlend@yandex.ru>
2025-12-10 13:39:05 +0300

    setup cpp/cmake

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+-- Set how many columns a tab character counts for on the screen
+vim.opt.tabstop = 4
+-- Set the number of spaces used for each step of indentation
+vim.opt.shiftwidth = 4
+-- TAB key inserts an actual tab character, not spaces
+vim.opt.expandtab = false
+-- Automatic indentation
+vim.opt.smartindent = true
+
+vim.opt.list = true
+
+-- Define the characters to use for different whitespace types
+vim.opt.listchars = {
+	eol = "¬", -- Character at the end of the line
+	tab = "» ", -- Character for a tab followed by a space
+	trail = "•", -- Character for trailing spaces
+	space = "·", -- Character for regular spaces (optional, can be very noisy)
+	nbsp = "%", -- Character for non-breaking spaces
+}
+
+-- Disable autoformat on save globally
+vim.g.autoformat = false
---
+++ lua/plugins/cmake.lua
@@ -0,0 +1,46 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = { ensure_installed = { "cmake" } },
+	},
+	{
+		"mason.nvim",
+		opts = { ensure_installed = { "cmakelang", "cmakelint" } },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				neocmake = {},
+			},
+		},
+	},
+	{
+		"Civitasv/cmake-tools.nvim",
+		lazy = true,
+		init = function()
+			local loaded = false
+			local function check()
+				local cwd = vim.uv.cwd()
+				if vim.fn.filereadable(cwd .. "/CMakeLists.txt") == 1 then
+					require("lazy").load({ plugins = { "cmake-tools.nvim" } })
+					loaded = true
+				end
+			end
+			check()
+			vim.api.nvim_create_autocmd("DirChanged", {
+			callback = function()
+				if not loaded then
+					check()
+				end
+			end,
+			})
+		end,
+		opts = {
+			cmake_build_directory = function()
+				return "build/${variant:buildType}"
+			end,
+			cmake_regenerate_on_save = false,
+		},
+	}
+}
